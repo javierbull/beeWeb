99e0bb8efbea7da6121bcdc5b06e333fbe859b4a
Yanet Parreño <funciones@bee.com.pe>
2019-06-19 15:20:51 -0500

    agrego filtro de cards

--- js/main.js
+++ js/main.js
@@ -27,3 +27,18 @@ $(document).ready(function() {
     owl.trigger('stop.owl.autoplay')
   })
 });
+
+
+// FUNCION FILTER GRID
+$(function() {
+var selectedClass = "";
+$(".filter").click(function(){
+selectedClass = $(this).attr("data-rel");
+$("#gallery").fadeTo(100, 0.1);
+$("#gallery div").not("."+selectedClass);
+setTimeout(function() {
+$("."+selectedClass).fadeIn().addClass('animation');
+$("#gallery").fadeTo(300, 1);
+}, 300);
+});
+});
